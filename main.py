460b4c3301e2b95361c5d0ea1c3cdced88cb9c71
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-06-12 21:51:01 -0400

    largest element in an array

---
+++ main.py
@@ -0,0 +1,17 @@
+# Largest Element in an Array
+
+# def largest( arr, n):
+#     largest = 0
+#     for a in range(n):
+#         if arr[a] > largest:
+#             largest = arr[a]
+#     return largest
+
+# time complexity is O(n)
+
+# ----------------------------------
+
+# Second Largest Element in an Array without sorting
+
+
+
