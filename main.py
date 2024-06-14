0cc68ebd51239cdc414c6148439785852fd4a5de
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-06-13 21:17:52 -0400

    second largest in array.

--- main.py
+++ main.py
@@ -13,5 +13,15 @@
 
 # Second Largest Element in an Array without sorting
 
+def second_largest(arr):
+    largest = arr[0]
+    second = -1
+    for i in range(len(arr)):
+        if arr[i] > largest:
+            second = largest
+            largest = arr[i]
+        elif arr[i] < largest and arr[i] > second:
+            second = arr[i]
+    return second
 
-
+print(second_largest([4,5,3,6,7,3,9,2,8,2]))
