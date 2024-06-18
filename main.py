be3b856e6e91fe0ae7ee8ae6ffa5bb0896d396cd
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-06-17 20:28:12 -0400

    485. Max Consecutive Ones

--- main.py
+++ main.py
@@ -137,6 +137,19 @@
 #
 # print(missingNumber([3,0,1]))
 
+# 485. Max Consecutive Ones
+def findMaxConsecutiveOnes(nums):
+    max_count = 0
+    current_count = 0
+
+    for num in nums:
+        if num == 1:
+            current_count += 1
+            max_count = max(max_count, current_count)
+        else:
+            current_count = 0
+
+    return max_count
 
 
-
+print(findMaxConsecutiveOnes([1, 1, 1, 1, 0, 0, 1]))
